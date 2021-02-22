9fe2c920fbc370ba5a174a8e25fdacbd6040bc68
Edward Hsu <st89382000@gmail.com>
2021-02-22 22:10:03 +0800

    加入專案檔案。

---
+++ EdwardHsu.Lab.CloudEvents/Program.cs
@@ -0,0 +1,22 @@
+﻿using CloudNative.CloudEvents;
+using CloudNative.CloudEvents.Extensions;
+
+using System;
+using System.Net.Mime;
+
+namespace EdwardHsu.Lab.CloudEvents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cloudEvent = new CloudEvent(
+                   "com.github.pull.create",
+                   new Uri("https://github.com/cloudevents/spec/pull/123"))
+            {
+                DataContentType = new ContentType("application/json"),
+                Data = "[]"
+            };
+        }
+    }
+}
